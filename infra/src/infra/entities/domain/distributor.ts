dd8312908e11ef0aedea7ad819da50c4954f7b2f
Pedro Souza <pedro.ads510@gmail.com>
2024-12-28 15:22:23 -0300

    stashing distributor class

--- infra/src/infra/entities/domain/distributor.ts
+++ infra/src/infra/entities/domain/distributor.ts
@@ -3,73 +3,77 @@ import { Category } from "./category";
 import { Order } from "./order";
 import { Product } from "./product";
 
-type DistributorProps = {
-  name: string,
-  address: string,
-  department: Category[],
-  description: string,
-  selledProducts: number,
-  distributionDiscount: number,
-  accepted: boolean,
-  products: Product[],
-  orders: Order[],
+enum status {
+  REJECTED,
+  PENDING,
+  APROVED,
 }
 
+type DistributorProps = {
+  name: string;
+  address: string;
+  status: status;
+  stockId: string;
+  department: Category[];
+  description: string;
+  selledProducts: number;
+  accepted: boolean;
+  products: Product[];
+  orders: Order[];
+};
+
 export class Distributor extends Entity<DistributorProps> {
-    private constructor(props: DistributorProps, id?: string){
-      super(props, id);
-    }
+  private constructor(props: DistributorProps, id?: string) {
+    super(props, id);
+  }
 
-    static create(props: DistributorProps, id?: string){
-      const distributor = new Distributor(props, id);
+  static create(props: DistributorProps, id?: string) {
+    const distributor = new Distributor(props, id);
 
-      return distributor;
-    }
+    return distributor;
+  }
 
-    public get name(): string {
-      return this.name
-    }
+  public get name(): string {
+    return this.name;
+  }
 
-    public get description(): string {
-      return this.description
-    }
+  public get description(): string {
+    return this.description;
+  }
 
-    public get brand(): string {
-      return this.brand
-    }
+  public get brand(): string {
+    return this.brand;
+  }
 
-    public get address(): string {
-      return this.address
-    }
+  public get address(): string {
+    return this.address;
+  }
 
-    public get department(): string[] {
-      return this.department
-    }
+  public get department(): string[] {
+    return this.department;
+  }
 
-    public get selledProducts(): number {
-      return this.selledProducts
-    }
+  public get selledProducts(): number {
+    return this.selledProducts;
+  }
 
-    public get selled(): number {
-      return this.selled
-    }
-    
-    public get distributionDiscount(): number {
-      return this.distributionDiscount
-    }
+  public get selled(): number {
+    return this.selled;
+  }
 
-    public get accepted(): boolean {
-      return this.accepted
-    }
+  public get distributionDiscount(): number {
+    return this.distributionDiscount;
+  }
 
-    public get products(): string[] {
-      return this.products
-    }
+  public get accepted(): boolean {
+    return this.accepted;
+  }
 
-    public get orders(): string[] {
-      return this.orders
-    }
+  public get products(): string[] {
+    return this.products;
+  }
 
-
-
+  public get orders(): string[] {
+    return this.orders;
+  }
 }
