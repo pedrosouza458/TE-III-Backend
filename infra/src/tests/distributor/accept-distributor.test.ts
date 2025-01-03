107b5b762691475df959e82113bcf151ddb92cd1
Pedro Souza <pedro.ads510@gmail.com>
2025-01-02 23:46:44 -0300

    updating distributor repository

--- infra/src/tests/distributor/accept-distributor.test.ts
+++ infra/src/tests/distributor/accept-distributor.test.ts
@@ -12,7 +12,7 @@ describe("AcceptDistributor use-case", () => {
     mockUserRepository = {
       checkAdmin: jest.fn().mockResolvedValue({
         message: "Authorized",
-      statusCode: 200,
+        statusCode: 200,
       }),
     } as unknown as UserRepository;
 
@@ -23,34 +23,24 @@ describe("AcceptDistributor use-case", () => {
       }),
     } as unknown as DistributorRepository;
 
-    acceptDistributor = new AcceptDistributor(mockUserRepository, mockDistributorRepository);
+    acceptDistributor = new AcceptDistributor(
+      mockUserRepository,
+      mockDistributorRepository
+    );
   });
 
   it("should accept the distributor if the user is an admin", async () => {
     const adminId = "admin-id";
     const distributorId = "distributor-id";
 
-    const response: Message = await acceptDistributor.execute(adminId, distributorId);
+    const response: Message = await acceptDistributor.execute(
+      adminId,
+      distributorId
+    );
 
     expect(response).toEqual({
       message: "Distributor accepted",
       statusCode: 200,
     });
   });
-
-  // it("should return an error if the user is not an admin", async () => {
-  //   const adminId = "non-admin-id";
-  //   const distributorId = "distributor-id";
-  //   mockUserRepository.checkAdmin.mockResolvedValue({
-  //     message: "User is not an admin",
-  //     statusCode: 400,
-  //   });
-
-  //   const response: Message = await acceptDistributor.execute(adminId, distributorId);
-
-  //   expect(response).toEqual({
-  //     message: "User is not an admin",
-  //     statusCode: 400,
-  //   });
-  // });
 });
