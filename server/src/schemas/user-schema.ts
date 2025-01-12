fb847dc171225291121db1bd11ddaf89619fe874
Pedro Souza <pedro.ads510@gmail.com>
2025-01-12 16:51:11 -0300

    adding more schemas

--- server/src/schemas/user-schema.ts
+++ server/src/schemas/user-schema.ts
@@ -1,12 +1,18 @@
 import z from "zod"
 
+export enum Role {
+  Admin = "Admin",
+  User = "User",
+}
+
 export const userSchema = z.object({
-	id: z.string().uuid(),
-	name: z.string(),
-	email: z.string(),
-	password: z.string(),
-	work: z.array(z.string().uuid()).optional(),
-	ownedWork: z.array(z.string().uuid()).optional(),
-})
+  id: z.string().uuid(),
+  name: z.string(),
+  email: z.string(),
+  password: z.string(),
+  role: z.enum(Object.values(Role) as [Role, ...Role[]]), // Ensure it's a tuple of strings
+  work: z.array(z.string().uuid()).optional(),
+  ownedWork: z.array(z.string().uuid()).optional(),
+});
 
 export type User = z.infer<typeof userSchema>
